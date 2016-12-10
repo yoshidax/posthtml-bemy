ab19acbd5d26e823869105e7a17500cad73dd084
yoshidax <takehiko.yoshida.z@gmail.com>
2016-12-11 02:28:57 +0900

    fix: element class setting fails if text node does not exist. #5

--- src/index.js
+++ src/index.js
@@ -21,9 +21,6 @@ module.exports = (opts) => {
   const patternElement = new RegExp(`(^| )(${opts.elementPrefix}(?!${opts.elementPrefix.slice(0, 1)})[^\\s]+)`)
 
   const walk = (node, blockName) => {
-    if (!node.content) {
-      return
-    }
     const existClass = !!node.attrs && !!node.attrs.class
     const isBlock = existClass && patternBlock.test(node.attrs.class)
     const isElement = existClass && patternElement.test(node.attrs.class)
@@ -38,9 +35,12 @@ module.exports = (opts) => {
       node.attrs.class = node.attrs.class.replace(patternElement, `$1${blockName}$2`)
       node.attrs.class = node.attrs.class.replace(patternModifire, `$1${blockName}${element}$2`)
     }
+    if (!node.content) {
+      return
+    }
     node.content.forEach((node) => {
-      const isBlock = node.attrs && node.attrs.class && patternBlock.test(node.attrs.class)
-      if (!isBlock) {
+      const isBlock = !!node.attrs && !!node.attrs.class && patternBlock.test(node.attrs.class)
+      if (node.tag && !isBlock) {
         walk(node, blockName)
       }
     })
